a841da290f2f8c37f4873ff55dfe59fa94f45a92
Hamza Shafee Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2023-06-24 22:02:21 +0300

    Hello

---
+++ src/main/java/com/example/sagsback/SagsBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.sagsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagsBackApplication.class, args);
+    }
+
+}
